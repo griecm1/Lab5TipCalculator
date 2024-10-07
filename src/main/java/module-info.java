6336c2c51be0dd5436c88bdb6ec4a626092742ca
chris <griecm1@farmingdale.edu>
2024-10-06 22:22:32 -0400

    hi

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.lab5 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.lab5 to javafx.fxml;
+    exports com.example.lab5;
+}
